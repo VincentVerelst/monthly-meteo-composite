f82686643e6c1bbeb0bcef3a0f6d9c6a90fed00e
Vincent Verelst <vincent.verelst@vito.be>
2025-08-05 11:58:49 +0200

    create a small batch job to test

---
+++ src/test_job/test_job.py
@@ -0,0 +1,17 @@
+import openeo
+
+c = openeo.connect('openeo.vito.be').authenticate_oidc()
+
+bbox = {'north': 52.5, 'south': 52.45, 'east': 13.4, 'west': 13.35, 'crs': 4326}
+
+s2 = c.load_collection(
+    collection_id="SENTINEL2_L2A",
+    spatial_extent=bbox,
+    temporal_extent=["2020-01-01", "2020-01-10"],
+    bands = ["B04"]
+)
+
+job = s2.execute_batch(
+    out_format="GTiff",
+    title="small test job for Conveyor",
+)
