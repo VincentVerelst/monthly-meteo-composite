e8131e044f03b7c134bcbb92ecd7374995ce55a7
Vincent Verelst <vincent.verelst@vito.be>
2025-09-22 14:56:58 +0200

    openeo now connects with oidc client credentials

--- src/test_job/test_job.py
+++ src/test_job/test_job.py
@@ -1,8 +1,19 @@
+import mimir_utils
 import openeo
 
 
 def test_job() -> None:
-    c = openeo.connect("openeo.vito.be").authenticate_oidc()
+    parameters_api = mimir_utils.MimirClient.connect(
+        data_product="monthly-meteo-composite", environment="experimentation"
+    )
+    product_parameters_api = parameters_api.aws.parameters.data_product
+
+    client_id = product_parameters_api.get("openeo_client_id")
+    client_secret = product_parameters_api.get("openeo_client_secret")
+
+    c = openeo.connect("openeo.vito.be").authenticate_oidc_client_credentials(
+        client_id=client_id, client_secret=client_secret, provider_id="terrascope"
+    )
 
     bbox = {"north": 52.5, "south": 52.45, "east": 13.4, "west": 13.35, "crs": 4326}
 
